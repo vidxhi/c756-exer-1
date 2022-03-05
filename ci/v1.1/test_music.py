54b1767410db8c28f23a0cc492e0d0b4a4ca41da
vla80 <vidxhi@gmail.com>
2022-03-05 12:51:41 -0800

    Update test_music.py

--- ci/v1.1/test_music.py
+++ ci/v1.1/test_music.py
@@ -60,3 +60,28 @@ def test_orig_artist_oa(mserv, m_id_oa):
     assert trc == 200
     trc, oa = mserv.read_orig_artist(m_id_oa)
     assert trc == 200 and oa == orig_artist
+
+
+def test_full_cycle(mserv):
+    # `mserv` is an instance of the `Music` class
+
+    # Performance at 2010 Vancouver Winter Olympics
+    song = ('k. d. lang', 'Hallelujah')
+    # Soundtrack of first Shrek film (2001)
+    orig_artist = 'Rufus Wainwright'
+    # Original recording from album "Various Positions" (1984)
+    orig_orig_artist = 'Leonard Cohen'
+
+    # Create a music record and save its id in the variable `m_id`
+    # ... Fill in the test ...
+    trc, m_id = mserv.create(song[0], song[1], orig_artist)
+    assert trc == 200
+
+    trc = mserv.write_orig_artist(m_id, orig_orig_artist)
+    assert trc == 200
+    trc, artist, title, oa = mserv.read(m_id)
+    assert (trc == 200 and artist == song[0] and title == song[1]
+            and oa == orig_orig_artist)
+
+    # The last statement of the test
+    mserv.delete(m_id)
